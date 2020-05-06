1519e47c6d31116a1d850f8c5de036bc5161169d
Luiz Eduardo <luiz.eduardo@acct.global>
2020-05-05 21:20:19 -0300

    shorthand module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 
 import Title from "./app";
 import React from "react";
-import { render } from "react-dom";
+import { render } from "./react-dom";
 
 render(React.createElement(Title), document.querySelector('[data-js="app"]'));
