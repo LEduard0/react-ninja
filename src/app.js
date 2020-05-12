fcf3cc9529ab74065cc3166f3f760989ba8901c4
Luiz Eduardo <luiz.eduardo@acct.global>
2020-05-12 20:05:38 -0300

    source map and preset react - msub 07

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 "use strict";
 
 import React from "react";
-import ReactDOM from "react-dom";
 
 var Title = React.createClass({
   render: function () {
-    return React.createElement("h1", null, "title");
+    return <h1>Titulo</h1>
   },
 });
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import Title from "./app";
 import React from "react";
-import { render } from "./react-dom";
+import { render } from "react-dom";
+import Title from "./app";
 
-render(React.createElement(Title), document.querySelector('[data-js="app"]'));
+render(<Title />, document.querySelector('[data-js="app"]'));
