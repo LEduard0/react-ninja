37b2ea2bec43058f2884f2329c36b3a1c3db1dc9
Luiz Eduardo <luiz.eduardo@acct.global>
2020-05-13 20:44:23 -0300

    html attributes on react

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ import Title from "./title";
 const App = React.createClass({
   render: function () {
     return (
-      <div>
+      <div className="container">
         <Title name="Luiz Edu" />
+        <label htmlFor="input">Input </label>
+        <input type="name" id="input" />
       </div>
     );
   },
