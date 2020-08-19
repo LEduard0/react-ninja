0630148f6df02ecad40076ff5bcfd4d54cccb8a1
Luiz Eduardo <luiz.eduardo@acct.global>
2020-08-19 20:42:23 -0300

    pure function lesson

--- src/title.js
+++ src/title.js
@@ -2,29 +2,13 @@
 
 import React from "react";
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: "MR.",
-      lastname: {
-        first: "Serious",
-        last: "Strange",
-      },
-    };
-  },
-  render: function () {
-    return (
-      <h1>
-        Hello{" "}
-        {this.props.name +
-          " " +
-          this.props.lastname.first +
-          " " +
-          this.props.lastname.last}{" "}
-        !
-      </h1>
-    );
-  },
-});
+const Title = ({ name, lastname }) => (
+  <h1>{`Hello ${name} ${lastname}`}</h1>
+);
+
+Title.defaultProps = {
+  name: "no",
+  lastname: "name"
+}
 
 export default Title;
